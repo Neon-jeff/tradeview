acf98846b52b997c5b37f1075eb180d7026b31cb
Neon-jeff <omaksteejay@gmail.com>
2024-02-08 11:33:53 +0100

    whitenoise

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static'),
-   os.path.join(BASE_DIR,'staticfiles')
 ]
 
 
