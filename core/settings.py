db34d59ba7eec0533e912ff87f32fe4231c4a31d
Neon-jeff <omaksteejay@gmail.com>
2024-02-09 16:18:38 +0100

    db fix

--- core/settings.py
+++ core/settings.py
@@ -82,17 +82,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
-DATABASES = {
+if env('DEV'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default':dj_database_url.parse(env('DATABASE_URL'))
-}
+    }
+
 
 
 # Password validation
