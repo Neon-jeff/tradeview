8eae92d4132d99b6b5876ae12534fd10f4659dd2
Neon-jeff <omaksteejay@gmail.com>
2024-03-30 02:48:50 +0100

    experts_followers

--- accounts/models.py
+++ accounts/models.py
@@ -14,6 +14,7 @@ class CopyTrader(models.Model):
     profit_share=models.CharField(null=True,blank=True,max_length=200)
     copy_amount=models.IntegerField(default=0,null=True,blank=True)
     image=CloudinaryField('image',null=True,blank=True)
+    followers=models.IntegerField(default=0,null=True,blank=True)
 
     def __str__(self):
         return f'{self.name} Expert Trader'
